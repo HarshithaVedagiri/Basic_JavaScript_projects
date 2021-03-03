3a2830231bbe0e454b6d46b2fb03ffc38e07b7cd
Harshitha Vedagiri <56275047+HarshithaVedagiri@users.noreply.github.com>
2021-03-03 09:16:50 -0800

    Add files via upload

---
+++ tasks.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+
+document.querySelector("form").onsubmit = () => {
+    const task = document.querySelector("#task").value;
+
+    const li = document.createElement('li');
+    li.innerHTML = task;
+
+    document.querySelector("#tasks").append(li);
+}
+
+
+});
